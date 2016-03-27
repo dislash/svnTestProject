71d04be42590e4bd06642ca066f481e555aeefb7
dislash <forjava@minamis-iMac.local>
2016-03-27 21:59:37 +0900

    commit

---
+++ src/com/dislash/study/StringUtil.java
@@ -0,0 +1,12 @@
+package com.dislash.study;
+
+public class StringUtil {
+	public static boolean checkEvenNumber(int num) {
+		int check = num %2;
+		if ( check == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ test/com/dislash/study/StringUtilTest.java
@@ -0,0 +1,14 @@
+package com.dislash.study;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void testCheckEvenNumber() {
+		assertTrue(StringUtil.checkEvenNumber(2));
+	}
+
+}
